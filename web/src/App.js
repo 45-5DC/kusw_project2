ea3044c7f3c2f5519d5ea4ed8ccee2f2ef6ab332
Hanjaemin <jm@Hanjaeminui-MacBookAir.local>
2022-12-20 06:46:57 +0900

    assd

--- web/src/App.js
+++ web/src/App.js
@@ -4,11 +4,12 @@ import ColumnChart from "./components/chart";
 import Rowbar from "./components/rowBar";
 import CountUp from "react-countup";
 import "./App.css";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "./styles.css";
 
 const App = () => {
   const [file, setFile] = useState({});
   const [prob, setProb] = useState(null);
-  const [value, setValue] = useState(0);
 
   const { getRootProps, getInputProps } = useDropzone({
     accept: "image/*",
@@ -27,13 +28,9 @@ const App = () => {
         body: formData,
       })
         .then((res) => res.json())
-        .then(
-          (data) => {
-            setProb(data);
-          }
-          // setProb(Math.round(parseFloat(data.prop) * 1000) / 1000)
-        );
-      console.log(prob);
+        .then((data) => {
+          setProb(data);
+        });
     },
   });
   const image = (
@@ -50,35 +47,60 @@ const App = () => {
     />
   );
 
+  // return (
+  //   <div className="main">
+  //     <div className="dropArea" {...getRootProps()}>
+  //       <input {...getInputProps()} />
+  //       <p className="text">Drag and Drop Here</p>
+  //     </div>
+  //     <div className="context">
+  //       {prob && image}
+  //       {prob && <p style={{ textAlign: "center" }}>생성되었을 확률</p>}
+  //       {/* {prob && (
+  //         <p className="percent">
+  //           {Math.round(parseFloat(prob.result) * 10000) / 100} %
+  //         </p>
+  //       )} */}
+  //       {prob && (
+  //         <CountUp
+  //           className="percent"
+  //           end={Math.round(parseFloat(prob.result) * 10000) / 100}
+  //           decimals={2}
+  //           suffix="%"
+  //         />
+  //       )}
+  //       {/* {prob && (
+  //         <ColumnChart prob={Math.round(parseFloat(prob.result) * 100) / 100} />
+  //       )}{" "} */}
+  //       {prob && (
+  //         <Rowbar prob={Math.round(parseFloat(prob.result) * 100) / 100} />
+  //       )}
+  //     </div>
+  //   </div>
+  // );
+
   return (
     <div className="main">
       <div className="dropArea" {...getRootProps()}>
         <input {...getInputProps()} />
         <p className="text">Drag and Drop Here</p>
       </div>
-      <div className="context">
-        {prob && image}
-        {prob && <p style={{ textAlign: "center" }}>생성되었을 확률</p>}
-        {/* {prob && (
-          <p className="percent">
-            {Math.round(parseFloat(prob.result) * 10000) / 100} %
-          </p>
-        )} */}
-        {prob && (
+
+      {prob && (
+        <div className="context">
+          {image}
+          <p style={{ textAlign: "center" }}>생성되었을 확률</p>
           <CountUp
             className="percent"
             end={Math.round(parseFloat(prob.result) * 10000) / 100}
             decimals={2}
             suffix="%"
           />
-        )}
-        {/* {prob && (
-          <ColumnChart prob={Math.round(parseFloat(prob.result) * 100) / 100} />
-        )}{" "} */}
-        {prob && (
-          <Rowbar prob={Math.round(parseFloat(prob.result) * 100) / 100} />
-        )}
-      </div>
+          <div>
+            <CircularProgressbar value={prob.result} strokeWidth="8" />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
